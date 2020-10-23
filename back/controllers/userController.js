7c9f942ecec1b4dd906ce8bd05bd5c49724e4a45
CamilaReyes97 <camila.reyesber97@gmail.com>
2020-10-22 20:43:14 -0500

    Actualizacion 22 de octubre

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,6 +1,9 @@
 /* Aqui es donde vamos a crear registros, modificaciones, eliminar */
 var User= require('../models/user'); //Treamos el modelo que acabamos de crear
 var bcrypt= require('bcrypt-nodejs'); //Incriptar las contraseñas
+var jwt= require('../helpers/jwt') //Requerimos el token
+
+
 
 function registrar(req , res) {
     var params= req.body; //Guarda la informacion de quien se esta registrando
@@ -38,6 +41,66 @@ function registrar(req , res) {
     }
 }
 
+function login(req, res) {
+    var data= req.body; // recibe el usuario y la contraseña para despues compararlos
+
+    //Si el correo existe, que lo traiga de la BD
+    User.findOne({email: data.email}, (err, user_data)=>{
+        if (err) {
+            // 500- el servidor de whatsapp se cayo
+            //404- NO se encuentra la persona o el mensaje
+            //200- cuando se envia el mensaje correctamente
+            res.status(500).send({message: "Error en el servidor"});
+        } else {
+            if (!user_data) { //user_data es el email que se ingreso en el login, si no se encuentra en la BD, no se ha registrado
+                res.status(404).send({message: "El correo NO esta registrado"})
+            }else{ //Si se encontro el correo en la BD
+                bcrypt.compare(data.password, user_data.password, function(err,check){
+                    if (check) { //si son iguales las contraseñas
+                        if (data.gettoken) {
+ 
+                            res.status(200).send({
+                                jwt: jwt.createToken(user_data), //llamamos el token
+                                user: user_data, //Me muestre la informacion del usuario
+                                message: 'Este usuario tiene un token'
+                            });
+
+                        } else {
+                            res.status(200).send({
+
+                                jwt: jwt.createToken(user_data),
+                                user: user_data, //Me muestre la informacion del usuario
+                                message: 'Este usuario NO tiene un token'
+                            });
+                        }
+                    } 
+
+                })
+
+            }
+        }
+
+    })
+}
+//Vamos a buscar un usuario para chatear con él o ella
+function get_user(req, res){
+
+        User.findById(id, (err, user)=>{
+            if (err) {
+                res.status(500).send({message: 'Error en el servidor'})
+            } else { //Si encuentra una coincidencia
+                if (user) { //si coincide, o existe
+                    res.status(200).send({user:user}) //muestre me los datos del usuario
+                } else { //No existe esa persona
+                    res.status(500).send({message: 'No existe un usuario con ese id'})
+                }
+            }
+        })
+}
+
+
 module.exports={
-    registrar
+    registrar,
+    login,
+    get_user
 }
--- back/routes/user.js
+++ back/routes/user.js
@@ -4,5 +4,8 @@ var app= express.Router(); //Me permite crear las rutas donde viajara la informa
 var userController= require('../controllers/userController') //Requerimos el archivo que acabamos de crear
 
 app.post('/registrar', userController.registrar) //Creamos la ruta que nos permitira registrar
+app.post('/login', userController.login) //Creamos la ruta login
+app.get('/usuario/:id', userController.get_user) //Creamos la ruta para traer la informacion de esa persona
+
 
 module.exports= app;
